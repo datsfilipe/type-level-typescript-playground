5ef3a920caccbc4efcaf92b1116496520e27651d
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-27 19:20:32 -0300

    feat(second lesson): lesson: Types are just data -- add challenges of the lesson

--- src/index.ts
+++ src/index.ts
@@ -81,3 +81,122 @@ namespace pipe2 {
   
   type test2 = Expect<Equal<typeof res2, boolean>>;
 }
+
+// 2. Second Lesson - Types are just data
+
+// 1. Type the `move` function so that the `direction` parameter can only be assigned to "backward" or "forward".
+namespace move {
+  function move(direction: "backward" | "forward") {
+    // some imaginary code that makes the thing move!
+    return direction
+  }
+
+  move("backward")
+  move("forward")
+
+  // @ts-expect-error: ❌ not supported
+  move("left")
+  // @ts-expect-error: ❌ not supported
+  move("right")
+}
+
+// 2. `pickOne` takes 2 arguments of potentially different types and return either one or the other at random. Make  generic!
+namespace pickOne {
+  function pickOne<A, B>(a: A, b: B): A | B {
+    return Math.random() > 0.5 ? a : b;
+  }
+
+  const res1 = pickOne(true, false);
+  type test1 = Expect<Equal<typeof res1, boolean>>;
+
+  const res2 = pickOne(1, 2);
+  type test2 = Expect<Equal<typeof res2, 1 | 2>>;
+
+  const res3 = pickOne(2, "some string");
+  type test3 = Expect<Equal<typeof res3, 2 | "some string">>;
+
+  const res4 = pickOne(true, 7);
+  type test4 = Expect<Equal<typeof res4, true | 7>>;
+}
+
+// 3. The `merge` function accepts an object of type `A` and an object of type `B`, and return an object with all properties of `A` and `B`. Make it generic!
+namespace merge {
+  function merge<A, B>(a: A, b: B): A & B {
+    return { ...a, ...b };
+  }
+
+  const res1 = merge({ name: "Bob" }, { age: 42 });
+  type test1 = Expect<Equal<typeof res1, { name: string } & { age: number }>>;
+
+  const res2 = merge({ greeting: "Hello" }, {});
+  type test2 = Expect<Equal<typeof res2, { greeting: string }>>;
+
+  const res3 = merge({}, { greeting: "Hello" });
+  type test3 = Expect<Equal<typeof res3, { greeting: string }>>;
+
+  const res4 = merge({ a: 1, b: 2 }, { c: 3, d: 4 });
+  type test4 = Expect<
+    Equal<typeof res4, { a: number; b: number } & { c: number; d: number }>
+  >;
+}
+
+// 4. Type `debounceFn` as a function with a `cancel` method on it.
+namespace debouncedFn {
+  let debouncedFn: (() => void) & { cancel: () => void };
+  debouncedFn = Object.assign(() => {}, { cancel: () => {} });
+
+  debouncedFn();
+  debouncedFn.cancel();
+
+  // ❌ `unknownMethod` does not exist on `debouncedFn`.
+  // @ts-expect-error
+  debouncedFn.unknownMethod();
+
+  // ❌ can't assign a string to `debouncedFn`.
+  // @ts-expect-error: ❌
+  debouncedFn = "Hello";
+}
+
+// 5. Type the `stringify` function to take any kind of input.
+namespace stringify {
+  function stringify<T>(input: T) {
+    return input instanceof Symbol ? input.toString() : `${input}`;
+  }
+
+  stringify("a string");    // ✅
+  stringify(12);            // ✅
+  stringify(true);          // ✅
+  stringify(Symbol("cat")); // ✅
+  stringify(20000n);        // ✅
+}
+
+// 6. Type the `exhaustive` function so that it cannot be called except in unreachable code branches.
+namespace exhaustive {
+  function exhaustive(...args: never) {}
+
+  const HOURS_PER_DAY = 24
+  // Since `HOURS_PER_DAY` is a `const`, the next condition can never happen
+  if (HOURS_PER_DAY !== 24) exhaustive(HOURS_PER_DAY);
+
+  // Outside of the condition, this should return a type error.
+  // @ts-expect-error ❌
+  exhaustive(HOURS_PER_DAY);
+
+  const exhautiveCheck = (input: 1 | 2) => {
+    switch (input) {
+      case 1: return "!";
+      case 2: return "!!";
+      // Since all cases are handled, the default branch is unreachable.
+      default: exhaustive(input);
+    }
+  }
+
+  const nonExhautiveCheck = (input: 1 | 2) => {
+    switch (input) {
+      case 1: return "!";
+      // the case where input === 2 isn't handled, so `exhaustive` shouldn't be called.
+      // @ts-expect-error ❌
+      default: exhaustive(input);
+    }
+  }
+}
